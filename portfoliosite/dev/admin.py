ef884471d20dfc7c872c68e3ec605a78251035a3
mirzobeck <99405198+mirzobeck@users.noreply.github.com>
2022-12-22 19:10:04 +0500

    Delete portfoliosite directory

--- portfoliosite/dev/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import about, work, skill, contact
-# Register your models here.
-
-
-admin.site.register(about)
-admin.site.register(work)
-admin.site.register(skill)
-admin.site.register(contact)
